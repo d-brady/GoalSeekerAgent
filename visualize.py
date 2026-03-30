00bc964c55d54bfb23e3ac683dae2121842ab52d
DBr <bradyds@outlook.de>
2026-03-30 15:24:21 +0200

    fix: Visualize now uses step counters as well

--- visualize.py
+++ visualize.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
 from simulation import BallSim
 
-sim = BallSim(x0=0, t_end=10, dt=0.1, noise_strength=1, seed=1, goal=(-0.5, 0.5))
+sim = BallSim(x0=0, t_end=10, dt=0.1, noise_strength=1, seed=2, goal=(-0.5, 0.5))
 
-while sim.t < sim.t_end:
+while sim.step_counter_less_than_max_steps():
     sim.step(u=0)
 
 fig, ax = plt.subplots()
